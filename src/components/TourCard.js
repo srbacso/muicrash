7566dc9a64454e23910a5266d560a3f5c74404e2
Stephen Bacso <stephen.bacso@gmail.com>
2022-10-20 11:25:26 -0400

    starting point

---
+++ src/components/TourCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/system/Box";
+
+const TourCard = () => {
+  return (
+    <Grid item xs={3}>
+      <Paper elevation={3}>
+        <img
+          className="img"
+          src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg"
+        />
+        <Typography variant="subtitle1" component="h2">
+          Immerse Into the Falls
+        </Typography>
+      </Paper>
+    </Grid>
+  );
+};
+
+export default TourCard;
