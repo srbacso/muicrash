34dc51f66205e0decb2c725f7a2f7d4fb28174d6
Stephen Bacso <stephen.bacso@gmail.com>
2022-10-20 21:30:56 -0400

    add to card

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -4,29 +4,78 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/system/Box";
 import { AccessTime } from "@mui/icons-material";
+import Rating from "@mui/material/Rating";
+import { createTheme } from "@mui/material";
+import ThemeProvider from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img
-          className="img"
-          src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg"
-          alt="niagra falls"
-        />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            Immerse Into the Falls
-          </Typography>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <AccessTime sx={{ width: 12.5 }} />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              5 Hours
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img
+            className="img"
+            src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg"
+            alt="niagra falls"
+          />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              Immerse Into the Falls
             </Typography>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <AccessTime sx={{ width: 12.5 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                5 Hours
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
+              <Rating
+                name="current-rating"
+                size="small"
+                value={3.5}
+                precision={0.5}
+                readOnly
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                3.5
+              </Typography>
+              <Typography variant="body3" component="p" marginLeft={1.5}>
+                (655 reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={0}>
+                From C$147
+              </Typography>
+            </Box>
           </Box>
-          <Box sx={{ display: "flex", alignItems: "center" }}></Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
