f9d480d11f1b5536de8fc7a5530115976b19df98
Stephen Bacso <stephen.bacso@gmail.com>
2022-10-24 12:08:59 -0400

    finished version

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,7 +1,56 @@
 import React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+import ImageCollage from "../components/ImageCollage";
+import CustomizedAccordions from "../components/accordian";
+import Paper from "@mui/material/Paper";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BasicModal from "../components/Modal";
 
 const Tour = () => {
-  return <div>Hello from Tour</div>;
+  return (
+    <Container sx={{ width: 900 }}>
+      <Typography variant="h3" componenet="h1" marginTop={3}>
+        Explore the world.
+      </Typography>
+      <Box marginTop={3} sx={{ display: "flex" }}>
+        <img
+          src="https://media.timeout.com/images/105124791/750/422/image.jpg"
+          alt="vegas"
+          height={325}
+        />
+        <ImageCollage />
+      </Box>
+      <Box>
+        <Typography variant="h6" componenet="h4" marginTop={3}>
+          About this ticket
+        </Typography>
+        <Typography variant="paragraph" componenet="p" marginTop={3}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
+          voluptates rem quos delectus debitis earum modi, ipsum veritatis.
+          Perferendis officiis nobis, aut hic praesentium nulla vero, possimus
+          omnis reprehenderit blanditiis quis incidunt minima voluptatibus illum
+          quam corporis libero fugiat doloremque. Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Exercitationem officiis commodi beatae
+          animi incidunt necessitatibus aut provident ad ex. Saepe!
+        </Typography>
+        <Typography variant="h6" componenet="h4" marginTop={3} marginBottom={2}>
+          Frequently asked questions.
+        </Typography>
+        <CustomizedAccordions />
+      </Box>
+      <Paper
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <BottomNavigation>
+          <BasicModal />
+        </BottomNavigation>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Tour;
